38b60e6fc41096e4040994029bbdc07d4addf793
hangalice2156 <hangalice2156@gmail.com>
2018-12-17 23:50:09 +0800

    ==Add basic template==

---
+++ utils.py
@@ -0,0 +1,27 @@
+import requests
+
+
+GRAPH_URL = "https://graph.facebook.com/v2.6"
+ACCESS_TOKEN = "EAAbkMw3Nx4YBAORAcz51WSV9ldgDJWmGKZApPREbd0oZAEYoEHiRCBh0jM96Bo8OY4KZAVZAWuB0XyDreDq6OZBAqZB84TnTnOGEVth5jYE8AVEwZB5jAW8mURuR4Ye0CC8TcOtR8Wuj313Cj9rnPKnmyFBDWyW51eIoOOSshDVrNwcgCBlnXMh"
+
+
+def send_text_message(id, text):
+    url = "{0}/me/messages?access_token={1}".format(GRAPH_URL, ACCESS_TOKEN)
+    payload = {
+        "recipient": {"id": id},
+        "message": {"text": text}
+    }
+    response = requests.post(url, json=payload)
+
+    if response.status_code != 200:
+        print("Unable to send message: " + response.text)
+    return response
+
+
+"""
+def send_image_url(id, img_url):
+    pass
+
+def send_button_message(id, text, buttons):
+    pass
+"""
